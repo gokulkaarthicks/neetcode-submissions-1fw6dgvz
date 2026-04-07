b734b13277bbeefa0b79743ed8bae4af40d8d2e1
gokulkaarthicks <34797231+gokulkaarthicks@users.noreply.github.com>
2026-04-06 20:48:29 -0400

    Add: depth-of-binary-tree - submission-4

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-4.py
@@ -0,0 +1,38 @@
+import random
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # if not root:
+        #     return 0
+        # return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
+
+        if not root:
+            return 0
+        
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            level += 1
+        
+        return level
+
+        # stack = [[root, 1]]
+
+        # res = 0
+
+        # while stack:
+        #     node, depth = stack.pop()
+
+        #     if node:
+        #         res = max(depth, res)
+        #         stack.append([node.left, depth + 1])
+        #         stack.append([node.right, depth + 1])
+        
+        # return res
